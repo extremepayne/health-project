440413ecb96d9638e80b2621362eb0948f5950a6
extremepayne <harrison.payne@gmail.com>
2019-04-20 14:35:50 -0600

    alcohol facts

---
+++ drugs_are_bad.py
@@ -0,0 +1,15 @@
+"""Say things about drugs being bad."""
+DRUG_FACTS = {
+    "alcohol": [
+        "Banning alcohol would save more lives than gun control.",
+        "1 in 12 US adults suffer from alcohol dependence. \
+        [facingaddiction.org]",
+        "1 in 4 US teens report binge drinking. \
+        [facingaddiction.org]",
+        "Every year, 2.8 million deaths are caused by alcohol. \
+        [facingaddiction.org]",
+        "40% of hosiptal beds in the US are being used to treat a \
+        alcohol-related condition. [facingaddiction.org]",
+        "Alcohol use can lead to dementia. [facingaddiction.org]",
+    ]
+}
