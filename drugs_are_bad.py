cd2ad566a0b3275c7b89f53b547c8aeedf950d94
extremepayne <harrison.payne@gmail.com>
2019-04-20 14:48:06 -0600

    smoking facts

--- drugs_are_bad.py
+++ drugs_are_bad.py
@@ -1,6 +1,14 @@
 """Say things about drugs being bad."""
 DRUG_FACTS = {
     "alcohol": [
+        "Banning alcohol would save more lives than gun control.",  # This
+        "Banning alcohol would save more lives than gun control.",  # one
+        "Banning alcohol would save more lives than gun control.",  # is
+        "Banning alcohol would save more lives than gun control.",  # really
+        "Banning alcohol would save more lives than gun control.",  # by
+        "Banning alcohol would save more lives than gun control.",  # far
+        "Banning alcohol would save more lives than gun control.",  # the
+        "Banning alcohol would save more lives than gun control.",  # best.
         "Banning alcohol would save more lives than gun control.",
         "1 in 12 US adults suffer from alcohol dependence. \
         [facingaddiction.org]",
@@ -11,5 +19,19 @@ DRUG_FACTS = {
         "40% of hosiptal beds in the US are being used to treat a \
         alcohol-related condition. [facingaddiction.org]",
         "Alcohol use can lead to dementia. [facingaddiction.org]",
-    ]
+    ],
+    "cigarettes": [
+        "Using cigarettes decreases your life expectancy by 10 years. \
+        [standaz.com/tobacco-facts]"
+        "Cigarettes contain at least 70 carcinogens (which cause cancer.) \
+        [standaz.com/tobacco-facts]",
+        "Tabacco use is the leading cause of preventable death -- worldwide. \
+        [standaz.com/tobacco-facts]",
+        "9 in 10 smokers started before they were 18. \
+        [standaz.com/tobacco-facts]",
+        "7 in 10 smokers want to quit. [standaz.com/tobacco-facts]",
+        "If the current trend continues, 1 in 13 americans who are teens now \
+        will die prematurely from smoking-related causes. \
+        [standaz.com/tobacco-facts]",
+    ],
 }
